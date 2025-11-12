8a35027cdc9ed0d536157a83e933df46b981945a
Rifatul <rifatul.ism@gmail.com>
2025-11-12 11:45:00 +0600

    implement Repository and IRepository

---
+++ BookShelf.API/Repository/IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace BookShelf.API.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T? GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
---
+++ BookShelf.API/Repository/Repository.cs
@@ -0,0 +1,25 @@
+﻿using BookShelf.API.Data;
+using BookShelf.API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelf.API.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+            _dbSet = _dbcontext.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll() => _dbSet.ToList();
+        public T? GetById(int id) => _dbSet.Find(id);
+        public void Add(T entity) => _dbSet.Add(entity);
+        public void Update(T entity) => _dbSet.Update(entity);
+        public void Delete(T entity) => _dbSet.Remove(entity);
+        public void Save() => _dbcontext.SaveChanges();
+    }
+}
